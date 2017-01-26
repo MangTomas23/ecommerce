a2d3d47a91581bf06b495e549b6d1bb999545361
Adrian Paul Matos <matos.adrianpaul@gmail.com>
2017-01-26 22:03:12 +0800

    Create login page

--- header.php
+++ header.php
@@ -3,5 +3,7 @@
   <head>
     <title><?php echo $page_title ?></title>
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/custom.css">
   </head>
   <body>
---
+++ login.php
@@ -0,0 +1,32 @@
+<?php
+$page_title = 'Login';
+include 'header.php';
+?>
+
+<div class="login-page">
+  <div class="login-container">
+    <div class="login-logo">
+      login
+    </div>
+      <form class="login-form" action="index.html" method="post">
+        <div class="form-group">
+          <input type="text" class="login-input form-control" name="name" value=""
+                 placeholder="email">
+        </div>
+        <div class="form-group">
+          <input type="text" class="login-input form-control" name="name" value=""
+                 placeholder="password">
+        </div>
+        <button type="button" class="login-button" name="button">
+          <i class="fa fa-chevron-right"></i>
+        </button>
+      </form>
+      <div class="login-form-footer">
+        <p>
+          new user? <a href="#">create new account</a>
+        </p>
+      </div>
+  </div>
+</div>
+
+<?php include 'footer.php'; ?>
