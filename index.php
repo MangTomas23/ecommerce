b94bbf6c4401c5a01c0c727ec6c8909f271e0400
Adrian Matos <matos.adrianpaul@gmail.com>
2017-01-28 09:24:16 +0800

    Separate navbar

--- index.php
+++ index.php
@@ -1,31 +1,14 @@
 <?php
 $page_title = 'Home';
 include 'header.php';
+include 'navbar.php';
 require 'Classes/Product.php';
 
 $product = new Product();
+
 ?>
 
-<nav class="navbar navbar-default">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#top-navbar">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Site Title</a>
-    </div>
-    <div class="collapse navbar-collapse" id="top-navbar">
-      <ul class="nav navbar-nav navbar-right">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#" class="shopping-cart-icon"><i class="fa fa-shopping-cart"></i></a></li>
-        <li><a href="#">My Account</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
+
 
 <div class="container">
   <h1 class="header-title">PRODUCTS</h1>
