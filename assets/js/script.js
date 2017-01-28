9f5a1631d741f13ec5c5091f11e06672eb43b240
Adrian Matos <matos.adrianpaul@gmail.com>
2017-01-28 13:25:52 +0800

    Set cart badge

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,31 @@
 $(document).ready( function() {
 
   $('#btnAddToCart').on('click', function() {
-    var cartItems = JSON.parse(localStorage.getItem('cart')) || [];
+    var cartItems = getCartItems();
     cartItems.push(parseInt($('#productId').text()));
     localStorage.setItem('cart', JSON.stringify(cartItems));
     console.log(cartItems);
 
+    setCartBadge();
     $('.info-box').toggleClass('show');
-    $('.cart-no').text(cartItems.length);
     setTimeout(function() {
       $('.info-box').toggleClass('show');
     }, 3000);
-
   });
+
+  function getCartCount() {
+    var cartItems = getCartItems();
+    return cartItems.length;
+  }
+
+  function getCartItems() {
+    return JSON.parse(localStorage.getItem('cart')) || [];
+  }
+
+  function setCartBadge() {
+    $('.cart-no').text(getCartCount());
+  }
+
+  setCartBadge();
 
 });
