bb76810fd7338b6f0f53cf06aac060fef964e111
Adrian Matos <matos.adrianpaul@gmail.com>
2017-01-28 09:21:36 +0800

    Display products to homepage

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 $page_title = 'Home';
 include 'header.php';
+require 'Classes/Product.php';
+
+$product = new Product();
 ?>
 
 <nav class="navbar navbar-default">
@@ -26,6 +29,27 @@ include 'header.php';
 
 <div class="container">
   <h1 class="header-title">PRODUCTS</h1>
+  <div class="product-list">
+
+    <?php
+      $products = $product->getAll();
+      foreach($products as $p) {
+    ?>
+      <div class="col-md-3 col-sm-6 no-pads">
+        <a class="product-container" href="product.php?id=<?php echo $p->id ?>">
+          <div class="product-image">
+            <img src="uploads/<?php echo $p->image ?>" alt="" />
+          </div>
+          <div class="product-info">
+            <p class="product-name"><?php echo $p->name ?></p>
+            <p class="product-price"><?php echo $p->price ?></p>
+          </div>
+        </a>
+      </div>
+    <?php
+      }
+    ?>
+  </div>
 </div>
 
 <?php include 'footer.php'; ?>
