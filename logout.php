2f6706dceaea4194425b5977670ae7b24d87a35b
Adrian Matos <matos.adrianpaul@gmail.com>
2017-01-29 11:10:58 +0800

    Fix logout

--- logout.php
+++ logout.php
@@ -1,8 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <script>
+      window.onload = function() {
+        localStorage.clear();
+        window.location.href = 'login.php';
+      }
+    </script>
+  </body>
+</html>
+
 <?php
 session_start();
 require 'Classes/Customer.php';
 
 $customer = new Customer();
 $customer->logout();
-$customer->redirect('login.php');
 ?>
